7ea75217a319d0d2eeaf7ed04d82e221bd64da23
Gerard <gerard@wide-eyes.it>
2015-07-15 09:21:28 +0200

    added some packages ignored by git

---
+++ js/templates/placeholder.js
@@ -0,0 +1,6 @@
+define(['handlebars.runtime'], function(Handlebars) {
+  Handlebars = Handlebars["default"];  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+return templates['placeholder.hbs'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div>\n	<svg version=\"1.1\" id=\"Layer_1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" x=\"0px\" y=\"0px\" width=\"600px\" height=\"300px\" viewBox=\"0 0 600 300\" enable-background=\"new 0 0 600 300\" xml:space=\"preserve\" preserveAspectRatio=\"none\">\n		<line fill=\"none\" stroke=\"#777\" stroke-miterlimit=\"10\" x1=\"600\" y1=\"0\" x2=\"0\" y2=\"300\"/>\n		<g>\n			<path fill=\"#777\" d=\"M599,1v298H1V1H599 M600,0H0v300h600V0L600,0z\"/>\n		</g>\n	</svg>\n</div>\n";
+  },"useData":true});
+});
---
+++ js/templates/widget_container.js
@@ -0,0 +1,6 @@
+define(['handlebars.runtime'], function(Handlebars) {
+  Handlebars = Handlebars["default"];  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+return templates['widget_container.hbs'] = template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+  return "<div class=\"grid\">\n	<h2>Recommended for you</h2>\n</div>";
+},"useData":true});
+});
