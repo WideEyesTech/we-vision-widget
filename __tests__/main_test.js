965e23878d2cf5fea417c610344ddcd3ea393b67
Gerard Clos <gerard@wide-eyes.it>
2015-09-22 15:52:54 +0200

    15.52

--- __tests__/main_test.js
+++ __tests__/main_test.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.sendStatus(200);
+  res.sendStatus(201);
 });
 app.listen('3000');
 app.use(express.static(__dirname));
