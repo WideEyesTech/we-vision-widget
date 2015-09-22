bd9203232f6e542785a787dcaf883d350f8b2d07
Gerard Clos <gerard@wide-eyes.it>
2015-09-22 16:32:59 +0200

    16.32

--- __tests__/main_test.js
+++ __tests__/main_test.js
@@ -1,47 +1,32 @@
+var app = require('../server.js');
 var request = require('supertest');
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
-
-app.use(express.static(__dirname));
 
 describe('main test', function () {
-    it('should return 200', function(done) {
-      request(app)
-        .get('/')
-        .expect(200)
-        .end(function(err, res){
-          console.log(res);
-          if (err) throw err;
-          done();
-        });
-    });
+  it('should return 200', function (done) {
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function (err, res) {
+        if (err) throw err;
+        done();
+      });
+  });
 
-    it('should check iframe is loaded with content', function (done) {
-      browser
-        .url('/index.html')
-        .waitForExist('#we-vision-iframe', 5000)
-        .then(function (bool) {
-          browser.frame('we-vision-iframe')
-            .then(function () {
-              browser
+  it('should check iframe is loaded with content', function (done) {
+    browser
+      .url('/')
+      .waitForExist('#we-vision-iframe', 5000)
+      .then(function (bool) {
+        browser.frame('we-vision-iframe')
+          .then(function () {
+            browser
               .waitForExist('.card', 5000)
               .isExisting('.card')
-                .then(function(bool) {
-                  bool.should.be.equal(true);
-                })
-                .call(done);
-            });
-        })
-    });
+              .then(function (bool) {
+                bool.should.be.equal(true);
+              })
+              .call(done);
+          });
+      });
   });
+});
