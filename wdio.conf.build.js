d2a3ffb25fabdb72eac3115061e35b71caac93b3
Gerard Clos <gerard@wide-eyes.it>
2015-09-22 19:24:01 +0200

    19.24

--- wdio.conf.build.js
+++ wdio.conf.build.js
@@ -73,7 +73,8 @@ exports.config = {
   //
   // Set a base URL in order to shorten url command calls. If your url parameter starts
   // with "/", the base url gets prepended.
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://localhost',
+  port: 3000,
   //
   // Default timeout for all waitForXXX commands.
   waitforTimeout: 5000,
