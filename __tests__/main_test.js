ff1c6907d14b78480c0e6cf5fb9de18f012837ad
Gerard Clos <gerard@wide-eyes.it>
2015-09-22 16:16:23 +0200

    16.16

--- __tests__/main_test.js
+++ __tests__/main_test.js
@@ -2,20 +2,30 @@ var request = require('supertest');
 var express = require('express');
 var app = express();
 
-app.get('/');
-app.listen('3000');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+
 app.use(express.static(__dirname));
 
 describe('main test', function () {
-    // it('should return 200', function(done) {
-    //   request(app)
-    //     .get('/')
-    //     .expect(200)
-    //     .end(function(err, res){
-    //       if (err) throw err;
-    //       done();
-    //     });
-    // });
+    it('should return 200', function(done) {
+      request(app)
+        .get('/')
+        .expect(200)
+        .end(function(err, res){
+          console.log(res);
+          if (err) throw err;
+          done();
+        });
+    });
 
     it('should check iframe is loaded with content', function (done) {
       browser
