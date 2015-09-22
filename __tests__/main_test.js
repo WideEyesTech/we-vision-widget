1afdde9ab58697394afc8a5bc783cf9f0136046b
Gerard Clos <gerard@wide-eyes.it>
2015-09-22 15:54:44 +0200

    15.54

--- __tests__/main_test.js
+++ __tests__/main_test.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.sendStatus(201);
+  res.sendStatus(200);
 });
 app.listen('3000');
 app.use(express.static(__dirname));
 
 describe('main test', function () {
-    it('test de prueba', function(done) {
+    it('should return 200', function(done) {
       request(app)
         .get('/')
         .expect(200)
@@ -19,21 +19,21 @@ describe('main test', function () {
         });
     });
 
-    // it('checks that the iframe is correctly loading', function (done) {
-    //   browser
-    //     .url('/')
-    //     .waitForExist('#we-vision-iframe', 5000)
-    //     .then(function (bool) {
-    //       browser.frame('we-vision-iframe')
-    //         .then(function () {
-    //           browser
-    //           .waitForExist('.card', 5000)
-    //           .isExisting('.card')
-    //             .then(function(bool) {
-    //               bool.should.be.equal(true);
-    //             })
-    //             .call(done);
-    //         });
-    //     })
-    // });
+    it('should check iframe is loaded with content', function (done) {
+      browser
+        .url('/')
+        .waitForExist('#we-vision-iframe', 5000)
+        .then(function (bool) {
+          browser.frame('we-vision-iframe')
+            .then(function () {
+              browser
+              .waitForExist('.card', 5000)
+              .isExisting('.card')
+                .then(function(bool) {
+                  bool.should.be.equal(true);
+                })
+                .call(done);
+            });
+        })
+    });
   });
