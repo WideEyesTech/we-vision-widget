51278946afa838e74e391b40d854d177cc3598d0
Gerard Clos <gerard@wide-eyes.it>
2015-09-22 18:36:27 +0200

    18.36

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ isPortTaken(3000, function (err, bool) {
   } else {
     app = express();
     app.get('/');
-    var server = app.listen(port, function () {
+    var server = app.listen(3000, function () {
       var host = server.address().address;
       var port = server.address().port;
       console.log('Example app listening at http://%s:%s', host, port);
