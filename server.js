303a603d253fa309a5e627f133bfdb6342dd6fd7
Gerard Clos <gerard@wide-eyes.it>
2015-09-22 19:19:56 +0200

    19.19

--- server.js
+++ server.js
@@ -1,33 +1,11 @@
 var express = require('express');
 var app = null;
 
-isPortTaken(3000, function (err, bool) {
-  if (bool) {
-    return;
-  } else {
-    app = express();
-    app.get('/');
-    var server = app.listen(3000, function () {
-      var host = server.address().address;
-      var port = server.address().port;
-      console.log('Example app listening at http://%s:%s', host, port);
-    });
-    app.use(express.static(__dirname));
-  }
+app = express();
+app.get('/');
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
 });
-
-function isPortTaken(port, fn) {
-  var net = require('net')
-  var tester = net.createServer()
-    .once('error', function (err) {
-      if (err.code != 'EADDRINUSE') return fn(err)
-      fn(null, true)
-    })
-    .once('listening', function () {
-      tester.once('close', function () {
-          fn(null, false)
-        })
-        .close()
-    })
-    .listen(port)
-}
+app.use(express.static(__dirname));
---
+++ start_sc_server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -ex
+
+node server.js
