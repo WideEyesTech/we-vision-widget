555ae783171f1934439be00b95492d2f324b3426
Gerard Clos <gerard@wide-eyes.it>
2015-09-23 13:20:45 +0200

    13.20

--- wdio.conf.build.js
+++ wdio.conf.build.js
@@ -72,7 +72,7 @@ exports.config = {
   //
   // Set a base URL in order to shorten url command calls. If your url parameter starts
   // with "/", the base url gets prepended.
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com/wd/hub',
   //
   // Default timeout for all waitForXXX commands.
   waitforTimeout: 10000,
