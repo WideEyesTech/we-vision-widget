e475e9e515258ab2c797f37acb25e7df557c7096
Gerard Clos <gerard@wide-eyes.it>
2015-09-23 13:26:45 +0200

    13.26

--- server.js
+++ server.js
@@ -3,9 +3,10 @@ var app = null;
 
 app = express();
 app.get('/');
-var server = app.listen(3000, '0.0.0.0', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+var server = app.listen(3000, 'http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com/wd/hub',
+  function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s', host, port);
+  });
 app.use(express.static(__dirname));
