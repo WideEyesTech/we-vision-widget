e36cb0a0af9d2d1e5269f8cc20b81b688c34b6e0
Gerard Clos <gerard@wide-eyes.it>
2015-09-23 16:38:41 +0200

    16.38

--- __tests__/main_test.js
+++ __tests__/main_test.js
@@ -1,19 +1,25 @@
 describe('main test', function () {
   it('should check iframe is loaded with content', function (done) {
-    browser
-      .url('/')
-      .waitForExist('#we-vision-iframe', 20000)
-      .then(function (bool) {
-        browser.frame('we-vision-iframe')
-          .then(function () {
-            browser
-              .waitForExist('.card', 20000)
-              .isExisting('.card')
-                .then(function (bool) {
-                  expect(bool).toBe(true);
-                })
-                .call(done);
-          });
-      });
+    browser.url('/')
+      .getTitle().then(function (title) {
+        expect(title).toEqual("WE Widget Test Page");
+      })
+      .call(done);
+    //   browser
+    //     .url('/')
+    //     .waitForExist('#we-vision-iframe', 20000)
+    //     .then(function (bool) {
+    //       browser.frame('we-vision-iframe')
+    //         .then(function () {
+    //           browser
+    //             .waitForExist('.card', 20000)
+    //             .isExisting('.card')
+    //               .then(function (bool) {
+    //                 expect(bool).toBe(true);
+    //               })
+    //               .call(done);
+    //         });
+    //     });
+    // });
   });
 });
