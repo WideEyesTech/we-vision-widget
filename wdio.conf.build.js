dedcf53b6567f09d961408668be6b082981fcdbe
Gerard Clos <gerard@wide-eyes.it>
2015-09-23 16:00:25 +0200

    16.00

--- wdio.conf.build.js
+++ wdio.conf.build.js
@@ -76,7 +76,7 @@ exports.config = {
   baseUrl: 'http://localhost:3000',
   //
   // Default timeout for all waitForXXX commands.
-  waitforTimeout: 10000,
+  waitforTimeout: 20000,
   //
   // Initialize the browser instance with a WebdriverIO plugin. The object should have the
   // plugin name as key and the desired plugin options as property. Make sure you have
@@ -117,7 +117,7 @@ exports.config = {
   jasmineNodeOpts: {
     //
     // Jasmine default timeout
-    defaultTimeoutInterval: 10000,
+    defaultTimeoutInterval: 20000,
     //
     // The Jasmine framework allows it to intercept each assertion in order to log the state of the application
     // or website depending on the result. For example it is pretty handy to take a screenshot everytime
