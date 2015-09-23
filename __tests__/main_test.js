d8d38b6eabefffd1d08417b772d06cdadb5f2782
Gerard Clos <gerard@wide-eyes.it>
2015-09-23 15:53:21 +0200

    15.53

--- __tests__/main_test.js
+++ __tests__/main_test.js
@@ -2,12 +2,12 @@ describe('main test', function () {
   it('should check iframe is loaded with content', function (done) {
     browser
       .url('/')
-      .waitForExist('#we-vision-iframe', 10000)
+      .waitForExist('#we-vision-iframe', 20000)
       .then(function (bool) {
         browser.frame('we-vision-iframe')
           .then(function () {
             browser
-              .waitForExist('.card', 10000)
+              .waitForExist('.card', 20000)
               .isExisting('.card')
                 .then(function (bool) {
                   expect(bool).toBe(true);
